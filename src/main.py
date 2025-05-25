e4723c9d457f51735d76073cdd51a80008aab33f
faelim <jgrand@live.ca>
2025-05-25 12:31:32 -0500

    Added static content generation and copying into public

--- src/main.py
+++ src/main.py
@@ -1,8 +1,34 @@
+import os
+import shutil
+
 from textnode import *
 
-def main():
-    text_node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+def prepare_directory(source, destination):
+    if not os.path.exists(source) or os.listdir(source) == []:
+        print(f"Source '{source}' directory either does not exist or is empty. Aborting as there's nothing to populate with.")
+        return
+    
+    if os.path.exists(destination):
+        print(f"Destination '{destination}' exists, but isn't empty. Recreating it.")
+        shutil.rmtree(destination)
+        os.mkdir(destination)
+    else:
+        print(f"Destination '{destination}' doesn't exist. Creating it.")
+        os.mkdir(destination)
 
-    print(text_node)
+    source_contents = os.listdir(source)
+    if source_contents != []:
+        for obj in source_contents:
+            obj_path = os.path.join(source, obj)
+            dest_path = os.path.join(destination, obj)
+            if os.path.isfile(obj_path):
+                print(f"Copying {obj_path} to {destination}.")
+                shutil.copy(obj_path, dest_path)
+            else:
+                print(f"Processing {obj_path}.")
+                prepare_directory(obj_path, dest_path)
+
+def main():
+    prepare_directory("static", "public")
 
 main()
